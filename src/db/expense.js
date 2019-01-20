b115e39ec46cc6fd682266b2facce2618edf200a
kisdani <kisdani0720@gmail.com>
2019-01-20 11:17:45 +0100

    I have added the new DB functions: get, insert, update, remove

--- src/db/expense.js
+++ src/db/expense.js
@@ -1,11 +1,39 @@
+const { ObjectId } = require('mongodb');
 const { getDB } = require('./index.js');
 
+const colletctionName = 'expenses';
+
 async function list() {
-    const result = await getDB().collection('expenses').find().toArray();
+    const result = await getDB().collection(colletctionName).find().toArray();
+    // további szűrési opciók a .find() után: .skip().limit().sort() ...
 
     return result;
 }
 
+async function get(id) {
+    const result = await getDB().collection(colletctionName).findOne({ _id : ObjectId(id) });
+
+    return result;
+}
+
+async function insert(data) {
+    await getDB().collection(colletctionName).insertOne(data);
+
+    return data;  // Azért küldjük vissza a datat, mert amikor sikeres a küldés nem a mongodb sikeresen elküldte üzenetét kapjuk meg, hanem az elküldött 'data'-t
+}
+
+async function update(id, data) {
+    await getDB().collection(colletctionName).updateOne({  _id : ObjectId(id)},  {$set: data}); // $set mondja meg, hogyan updatelje. data tartalma pl: {amount : 5000}
+
+    return data;  // Azért küldjük vissza a datat, mert amikor sikeres a küldés nem a mongodb sikeresen elküldte üzenetét kapjuk meg, hanem az elküldött 'data'-t
+}
+
+async function remove(id) {
+    await getDB().collection(colletctionName).deleteOne({  _id : ObjectId(id)}); // $set mondja meg, hogyan updatelje. data tartalma pl: {amount : 5000}
+
+}
+
+
 module.exports = {
-    list,
+    list, get, insert, update, remove,
 }
--- src/web/expenseHandler.js
+++ src/web/expenseHandler.js
@@ -1,8 +1,24 @@
+const joi = require('joi');
 const expenses = require('../db/expense');
+const logger = require('../logger');
+
+const expenseSchema = joi.object({
+    title: joi.string().required(),
+    amount: joi.number().min(1).required(),
+}).unknown().required();
 
 
 async function get(req, res) {
+    const expenseID = req.params.id;
+
+    const result = await expenses.get(expenseID);
+
+    if(!result){
+        res.status(404);
+        res.end();
+    }
     
+    res.send(result);
 }
 
 async function list(req, res) {
@@ -13,14 +29,50 @@ async function list(req, res) {
 
 async function insert(req, res) {
     
+    //logger.info(JSON.stringify(req.body));  // logger nehezen kezeli a json fileokat, ezért át kell alakítani őket.
+
+    try{
+        joi.attempt(req.body, expenseSchema);
+    }
+    catch(err) {
+        res.status(400);
+        res.send(err.details[0].message); // ezt a hiba üzenetet a joi fogja dobni 
+    }
+
+    const result = await expenses.insert(req.body);
+
+    res.status(201); //Created status
+    res.send(result);
 }
 
 async function update(req, res) {
+        
+        //logger.info(JSON.stringify(req.body));  // logger nehezen kezeli a json fileokat, ezért át kell alakítani őket.
+        //Hasonló a felépítése az Update-nek, m int az insert-nek. Csak itt elküldjük a módosítandó elem ID-ját is.
+
+        try{
+            joi.attempt(req.body, expenseSchema);
+        }
+        catch(err) {
+            res.status(400);
+            res.send(err.details[0].message); // ezt a hiba üzenetet a joi fogja dobni 
+        }
     
+        const expenseID = req.params.id;
+
+        const result = await expenses.update(expenseID, req.body);
+    
+        res.send(result);
 }
 
 async function remove(req, res) {
     
+    const expenseID = req.params.id;
+
+    await expenses.remove(expenseID); 
+
+    res.status(204); // No content
+    res.end();
 }
 
 module.exports = {
